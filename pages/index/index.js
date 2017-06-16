6749a9a5c7ca10ce7aa9b55ae20df3b962f98d4a
changchaofan <changchaofan@baoyinxiaofei.com>
2017-06-16 17:45:46 +0800

    A 新增 添加微信小程序客户端sdk

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,40 @@
 //index.js
 //获取应用实例
-var cha = []
-var total = 0
-var pp = []
-var app = getApp()
+var cha = [];
+var total = 0;
+var pp = [];
+var app = getApp();
+
+// 引入 QCloud 小程序增强 SDK
+var qcloud = require('../../vendor/qcloud-weapp-client-sdk/index');
+
+// 引入配置
+var config = require('../../config');
+
+// 显示繁忙提示
+var showBusy = text => wx.showToast({
+  title: text,
+  icon: 'loading',
+  duration: 10000
+});
+
+// 显示成功提示
+var showSuccess = text => wx.showToast({
+  title: text,
+  icon: 'success'
+});
+
+// 显示失败提示
+var showModel = (title, content) => {
+  wx.hideToast();
+
+  wx.showModal({
+    title,
+    content: JSON.stringify(content),
+    showCancel: false
+  });
+};
+
 Page({
   data: {
     sum: null,
@@ -11,20 +42,29 @@ Page({
     num: null,
     perfect: [],
     tried: false,
-    addInputFocus: false
+    addInputFocus: false,
+    loginUrl: config.service.loginUrl,
+    requestUrl: config.service.requestUrl,
+    tunnelUrl: config.service.tunnelUrl,
+    tunnelStatus: 'closed',
+    tunnelStatusText: {
+      closed: '已关闭',
+      connecting: '正在连接...',
+      connected: '已连接'
+    }
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   //change sum
-  changeSum: function(e) {
+  changeSum: function (e) {
     var value = e.detail.value
-      // if (value == "") {
-      //   value = 0;
-      // }
+    // if (value == "") {
+    //   value = 0;
+    // }
     if (value) {
       this.setData({
         sum: value
@@ -33,9 +73,9 @@ Page({
     }
 
   },
-  onBlur: function(e) {
+  onBlur: function (e) {
     var value = e.detail.value
-    if(value!=null){
+    if (value != null) {
 
     }
     // if (e.target.id == "sum") {
@@ -45,20 +85,20 @@ Page({
     // }
   },
   //on input
-  onInput: function(e) {
+  onInput: function (e) {
     var value = e.detail.value
     // if(value==""){
     //   value = 0;
     // }
-    if(value==""){
-      value=null
+    if (value == "") {
+      value = null
     }
     this.setData({
       num: value
     })
   },
   //add number
-  addNumber: function(e) {
+  addNumber: function (e) {
     // if (this.data.num == null || this.data.num == "") {
     //   this.setData({
     //     num: 0
@@ -83,7 +123,7 @@ Page({
     })
   },
   //delete last number
-  deleteNumber: function(e) {
+  deleteNumber: function (e) {
     if (this.data.array.length > 0) {
       this.data.array.pop()
       this.setData({
@@ -93,7 +133,7 @@ Page({
     }
   },
   //clear number
-  clearNumber: function(e) {
+  clearNumber: function (e) {
     total = null
     pp = []
     cha = []
@@ -107,7 +147,7 @@ Page({
     })
   },
   //coushu
-  coushu: function() {
+  coushu: function () {
     // wx.showLoading({
     //   title: '计算中...',
     //   mask: true
@@ -131,13 +171,43 @@ Page({
     resetInput()
     // wx.hideLoading()
   },
-  resetInput:function(){
-this.setData({
+  resetInput: function () {
+    this.setData({
       array: [],
       sum: null,
       num: num
     })
-  }
+  },
+  /**
+     * 点击「请求」按钮，测试带会话请求的功能
+     */
+  doRequest() {
+    // showBusy('正在请求');
+    console.log(config.service.loginUrl);
+    qcloud.setLoginUrl(config.service.loginUrl);
+    // qcloud.request() 方法和 wx.request() 方法使用是一致的，不过如果用户已经登录的情况下，会把用户的会话信息带给服务器，服务器可以跟踪用户
+    qcloud.request({
+      // 要请求的地址
+      url: this.data.requestUrl,
+
+      // 请求之前是否登陆，如果该项指定为 true，会在请求之前进行登录
+      login: true,
+
+      success(result) {
+        showSuccess('请求成功完成');
+        console.log('request success', result);
+      },
+
+      fail(error) {
+        showModel('请求失败', error);
+        console.log('request fail', error);
+      },
+
+      complete() {
+        console.log('request complete');
+      }
+    });
+  },
 })
 
 function zuhe(n, m, a, re) {
@@ -156,8 +226,6 @@ function zuhe(n, m, a, re) {
         sum = parseFloat(sum) + parseFloat(re[j]);
       }
       console.log("sum=" + sum)
-        //				System.out.println("sum=" + sum);
-        //				System.out.println("需要凑的额度=" + (total - sum));
       if (total > sum) {
         cha.push(total - sum);
       }
@@ -167,7 +235,7 @@ function zuhe(n, m, a, re) {
         // pp[ppl]=re;
 
         pp.push(re.slice(0))
-          // console.log("ppl="+ppl+" pp[ppl]="+pp[ppl]);
+        // console.log("ppl="+ppl+" pp[ppl]="+pp[ppl]);
         console.log("pp:" + pp);
         // ppl++;
       }
